3981885644db9cfdd928c2b619c0db22ab1fb53b
Expua1 <badsnake1@gmail.com>
2024-03-11 16:01:31 +0200

    init

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '5336424335:AAGk0uyo0qqRCrKgvr2J7GrYKK1S0MF8878';
-const webAppUrl = 'https://ornate-selkie-c27577.netlify.app';
+const token = '7184725401:AAEZHG_PCzgJAEuJ1s0Cay62qjhBzFbIsE8';
+const webAppUrl = 'https://mytrycrea.netlify.app/';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
